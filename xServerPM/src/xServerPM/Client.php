5f3578e30fd14283df28694eb12d8e231947695e
Ken Piper <kealper@gmail.com>
2017-08-12 21:10:29 -0500

    Make sure book editing doesn't show in socialspy

--- xServerPM/src/xServerPM/Client.php
+++ xServerPM/src/xServerPM/Client.php
@@ -5,7 +5,7 @@ namespace xServerPM;
 use pocketmine\Thread;
 
 class Client extends Thread {
-	
+
 	const PACKET_NOOP             = 0;
 	const PACKET_SERVER_DC        = 1;
 	const PACKET_SERVER_NAME      = 2;
@@ -27,11 +27,11 @@ class Client extends Thread {
 	const PACKET_PLAYER_HELPOP    = 18;
 	const PACKET_SERVER_COMMAND   = 19;
 	const PACKET_PLAYER_OPCHAT    = 20;
-	
+
 	public $messageData, $messageType, $messagePerms;
 	private $socket;
 	private $address, $port, $name, $formats;
-	
+
 	public function __construct($address, $port, $name, $formats) {
 		$this->address = $address;
 		$this->port = $port;
@@ -40,7 +40,7 @@ class Client extends Thread {
 		$this->messageType = -1;
 		$this->start();
 	}
-	
+
 	public function sendMessage($messageData, $messagePerms = "xserver.message.recieve", $messageType = 0) {
 		$this->messageData = $messageData;
 		$this->messagePerms = $messagePerms;
@@ -52,7 +52,7 @@ class Client extends Thread {
 		$this->messagePerms = "";
 		$this->messageType = -1;
 	}
-	
+
 	public function format($packet, $type) {
 		$message = $packet->getFormat($type);
 		$message = str_replace("{message}", $packet->getArg("MESSAGE"), $message);
@@ -61,12 +61,12 @@ class Client extends Thread {
 		$message = preg_replace("/&([0-9a-frlmnok])/i", "§$1", $message);
 		return $message;
 	}
-	
+
 	public function send(Packet $packet) {
 		$packet->setFormats($this->formats);
 		socket_write($this->socket, $packet->toString());
 	}
-	
+
 	public function run() {
 		$this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 		socket_set_option($this->socket, SOL_SOCKET, SO_KEEPALIVE, 1);
@@ -89,13 +89,13 @@ class Client extends Thread {
 				if ($packet == "") {
 					continue;
 				}
-				
+
 				$packet = new Packet(json_decode($packet));
 				if (json_last_error() != JSON_ERROR_NONE) {
 					$this->sendMessage("Failed to decode packet!", "", 0);
 					continue;
 				}
-				
+
 				switch ($packet->type) {
 				case self::PACKET_NOOP:
 					continue;
@@ -168,21 +168,23 @@ class Client extends Thread {
 					break;
 
 				case self::PACKET_PLAYER_SOCIALSPY:
-					$this->sendMessage($this->format($packet, "SOCIALSPY"), "essentials.socialspy", 1);
+					if ($packet->getArg("TYPE") == "social") {
+						$this->sendMessage($this->format($packet, "SOCIALSPY"), "essentials.socialspy", 1);
+					}
 					break;
 
-				case self::PACKET_PLAYER_HELPOP:	
+				case self::PACKET_PLAYER_HELPOP:
 					$this->sendMessage($this->format($packet, "HELPOP"), "essentials.helpop.receive", 1);
 					break;
-				
-				case self::PACKET_PLAYER_OPCHAT:	
+
+				case self::PACKET_PLAYER_OPCHAT:
 					$this->sendMessage($this->format($packet, "OPCHAT"), "xserver.opchat.receive", 1);
 					break;
 
 				case self::PACKET_SERVER_COMMAND:
 					$this->sendMessage($packet->args["MESSAGE"], "", 2);
 					break;
-				
+
 				default:
 					$this->sendMessage("Unhandled packet type!", "", 0);
 					continue;
@@ -190,15 +192,15 @@ class Client extends Thread {
 			}
 		}
 	}
-	
+
 }
 
 class Packet {
-	
+
 	public $args;
 	public $type;
 	public $formats;
-	
+
 	public function __construct() {
 		if (func_num_args() == 1) {
 			$this->type = func_get_arg(0)->type;
@@ -210,47 +212,47 @@ class Packet {
 		$this->args = func_get_arg(1);
 		$this->formats = [];
 	}
-	
+
 	public function getType() {
 		return $this->type;
 	}
-	
+
 	public function setType($type) {
 		$this->type = $type;
 	}
-	
+
 	public function getArg($arg) {
 		return $this->args[$arg];
 	}
-	
+
 	public function setArg($arg, $value) {
 		$this->args[$arg] = $value;
 	}
-	
+
 	public function getArgs() {
 		return $this->args;
 	}
-	
+
 	public function setArgs($args) {
 		$this->args = $args;
 	}
-	
+
 	public function getFormat($format) {
 		return $this->formats[$format];
 	}
-	
+
 	public function setFormat($format, $value) {
 		$this->formats[$format] = $value;
 	}
-	
+
 	public function getFormats() {
 		return $this->formats;
 	}
-	
+
 	public function setFormats($formats) {
 		$this->formats = $formats;
 	}
-	
+
 	public function toString() {
 		return json_encode($this);
 	}
